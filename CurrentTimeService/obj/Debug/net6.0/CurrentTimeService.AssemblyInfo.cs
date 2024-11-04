951b7502c5ae0740c1b94ddf2190cf92af43cdc7
kathripdm <kadirpdm@gmail.com>
2024-11-04 07:45:59 +0530

    add pst timezone in dev

--- CurrentTimeService/obj/Debug/net6.0/CurrentTimeService.AssemblyInfo.cs
+++ CurrentTimeService/obj/Debug/net6.0/CurrentTimeService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CurrentTimeService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6e0fc2b77e43d557e9ec41e22b33789d406db6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29340ab3bbacf1b0fc3e5e7d43897c2049398ba3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CurrentTimeService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CurrentTimeService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
